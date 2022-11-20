f2685d99bf20baf13a88b0586ad49d97758e2aa7
Jimmyluluu <luu21@kimo.com>
2022-11-21 00:40:23 +0800

    add 指令集

--- exportLocal.py
+++ exportLocal.py
@@ -1,20 +1,17 @@
-import os
-import random
-import cv2
-
+import os, random, cv2, click, argparse
 # 資料處理輸出時間檔案輸出秒數集時間以及座標
 # 輸出格式為（'時間'：['0','x','y','h','w']）第一項0無意義
 
-def datas_handle():
+def datas_handle(mp4Name, coordinateFolder):
 #--------------開啟影片檔並求出fps------------------#
-    vid_cap = cv2.VideoCapture('drop.mp4') 
+    vid_cap = cv2.VideoCapture(mp4Name) 
     fps = vid_cap.get(cv2.CAP_PROP_FPS)
 #------------------------------------------------#
     datas = {}
     count = 0 #秒數
     filesortNum = []
     folderName = ""
-    folderPath = "drop"
+    folderPath = coordinateFolder
     listdir = os.listdir(folderPath)
 
     for l in listdir:
@@ -29,15 +26,14 @@ def datas_handle():
 
         f = open(filepath)
         for line in f:
-                line = line.split(' ')
-                if len(line) > 4:
-                    line[4] = line[4].replace("\n", "")
-                text.append(line)
-
+            line = line.split(' ')
+            if len(line) > 4:
+                line[4] = line[4].replace("\n", "")
+            text.append(line)
         datas.update({str(round(file/fps,2)) + "s": text})
         count = count + 1
         f.close
-    return datas
+    return datas #輸出資料夾
 
 def anxiety_status(datas):
     for data in datas:
@@ -45,7 +41,14 @@ def anxiety_status(datas):
 
 
 if __name__ == '__main__':
-    datas = datas_handle()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mp4Name", type=str, help='file_name1') #輸入mp4檔案取得fps
+    parser.add_argument("coordinateFolder", type=str, help='file_name1') #輸入座標資料夾
+    args = parser.parse_args()
+    mp4Name = args.mp4Name
+    coordinateFolder = args.coordinateFolder
+
+    datas = datas_handle(mp4Name, coordinateFolder)
     for data in datas:
         print(data,end=":")
         print(datas[data])
